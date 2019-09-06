5faae449c10897b1109eb265e3985f378d808a30
lg10 <1164048272@qq.com>
2019-09-07 00:01:04 +0800

    9月6日更新
    1.增加刷新自动更换背景
    2.将登录框大体成型
    3.增加版权信息
    4.文字艺术字还在考虑，暂无更好效果

---
+++ login/js/changeImg.js
@@ -0,0 +1,27 @@
+// var curIndex = 0;
+//时间间隔(单位毫秒)，每秒钟显示一张，数组共有3张图片放在img文件夹下
+// var timeInterval = 1000;
+var currentImg = Math.floor(Math.random() * 3);
+//定义一个存放照片位置的数组，可以放任意个，在这里放3个
+var arr = new Array();
+arr[0] = "C:/Users/zahui/Desktop/Java%20web/login/img/hhh.jpg";
+arr[1] = "C:/Users/zahui/Desktop/Java%20web/login/img/sss.jpg";
+arr[2] = "C:/Users/zahui/Desktop/Java%20web/login/img/718412.jpg";
+changeImg();
+// setInterval(changeImg, timeInterval);
+function changeImg() {
+//     // if (curIndex > arr.length - 2) {
+//     //     curIndex = 0;
+//     // } else {
+//     //     curIndex += 1;
+//     // }
+     document.body.style.backgroundImage = "url(" + arr[currentImg] + ")";  //显示对应的图片
+}
+
+// function changeImg2() {
+
+//     var img = document.getElementById("myDiv");
+
+//     img.style.backgroundImage = "url(img/" + img[currentImg] + ")"; //这里特别要注意引号的使用，因为img[curentImg]是个变量，所以用"+变量+"
+
+// }
